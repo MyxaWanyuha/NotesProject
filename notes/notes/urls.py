9103a15f8eadbe38a8044353d0ad1eaa03775bbb
Ivan Sablin <sablin.2001@inbox.ru>
2022-12-08 17:21:35 +0500

    login, logout, registration with logina and password

--- notes/notes/urls.py
+++ notes/notes/urls.py
@@ -1,16 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 
-from notesapp.views import auth, index, add_note, delete_note, NoteUpdateView, search
+from notesapp.views import index, add_note, delete_note, NoteUpdateView, search, RegisterFormView, \
+    logout_view, login_request
 
 urlpatterns = [
     path('', index),
     path('admin/', admin.site.urls),
     re_path('', include('social_django.urls', namespace='social')),
-    path('auth/', auth),
     path('addnote/', add_note),
     path('deletenote/', delete_note),
     re_path(r'^tinymce/', include('tinymce.urls')),
     path('<int:pk>/update', NoteUpdateView.as_view()),
     path('search/', search),
+    path('register/', RegisterFormView.as_view()),
+    path('login/', login_request),
+    path('logout/', logout_view),
 ]
--- notes/notesapp/views.py
+++ notes/notesapp/views.py
@@ -1,19 +1,68 @@
+import sys
+
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.db.models import Q
 from django.shortcuts import render, redirect
-from django.views.generic import UpdateView, CreateView, ListView
-
+from django.views.generic import UpdateView, FormView, CreateView
 from notesapp.forms import NoteForm
 from notesapp.models import Note
+from django.contrib import messages
+
+def logout_view(request):
+    if request.user.is_authenticated is False:
+        return redirect('/login')
+    logout(request)
+    return redirect('/login')
+
+
+class RegisterFormView(CreateView):
+    form_class = UserCreationForm
+    success_url = '/login'
+    template_name = 'register.html'
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated is True:
+            return redirect('/')
+        return super(RegisterFormView, self).get(self, request, args, kwargs)
+
+    def form_valid(self, form):
+        form.save()
+        return super(RegisterFormView, self).form_valid(form)
+
+    def form_invalid(self, form):
+        return super(RegisterFormView, self).form_invalid(form)
+
+
+def login_request(request):
+    if request.user.is_authenticated is True:
+        return redirect('/')
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect("/")
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request=request, template_name="login.html", context={"login_form": form})
 
 
 def search(request):
     error = ''
     if request.user.is_authenticated is False:
-        return redirect('/auth')
+        return redirect('/login')
     if request.method == 'GET':
         query = request.GET.get("q")
         context = {
-         'notes': Note.objects.filter(Q(title__iregex=query) | Q(body__iregex=query))
+            'notes': Note.objects.filter(Q(title__iregex=query) | Q(body__iregex=query))
         }
         return render(request, 'index.html', context)
     else:
@@ -31,17 +80,14 @@ class NoteUpdateView(UpdateView):
         return context
 
 
-def auth(request):
-    return render(request, 'oauth.html')
-
-
 def index(request):
     error = ''
+    print(request.user)
     if request.user.is_authenticated is False:
-        return redirect('/auth')
+        return redirect('/login')
 
     context = {
-         'notes': Note.objects.filter(owner=request.user).order_by('-id')
+        'notes': Note.objects.filter(owner=request.user).order_by('-id')
     }
     return render(request, 'index.html', context)
 
@@ -49,7 +95,7 @@ def index(request):
 def add_note(request):
     error = ''
     if request.user.is_authenticated is False:
-        return redirect('/auth')
+        return redirect('/login')
 
     if request.method == 'POST':
         form = NoteForm(request.POST)
@@ -73,7 +119,7 @@ def add_note(request):
 
 def delete_note(request):
     if request.user.is_authenticated is False:
-        return redirect('/auth')
+        return redirect('/login')
 
     if request.method == 'POST':
         id = request.POST.get('id')
